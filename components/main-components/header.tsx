d8669cf6d0b99cfe55e52787b30246b0c6fc4132
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-04 18:21:10 +0800

    change colors

--- components/main-components/header.tsx
+++ components/main-components/header.tsx
@@ -9,7 +9,7 @@ import ProfileDropdown from "./profileDropdown";
 
 export default function Header() {
   return (
-    <header className="fixed z-10 w-full flex items-center justify-between h-16 border border-b-[#CAC2FF80] px-10 backdrop-blur-md shadow-sm">
+    <header className="fixed z-50 w-full flex items-center justify-between h-16 border border-b-[#CAC2FF80] px-10 backdrop-blur-md shadow-sm">
       <div className="flex items-center gap-x-14">
         <Image src={logo} alt="logo" height={150} width={150} />
         <ul className="flex gap-x-12">
@@ -17,8 +17,8 @@ export default function Header() {
           <Link href={"#services"}>Services</Link>
         </ul>
       </div>
-      <div className="border  w-2/5 py-2 h-9  border-slate-300  rounded-full flex items-center text-center bg-[#EFECFF]">
-        <IoSearchOutline className="mx-3 text-2xl " />
+      <div className="border  w-2/5 py-2 h-9  border-[#624ced]  rounded-full flex items-center text-center bg-[#EFECFF] ">
+        <IoSearchOutline className="mx-3 text-2xl text-[#624ced]" />
         <input
           placeholder="Search Gallery Feed"
           className="bg-transparent  w-full h-4 text-sm outline-none "
@@ -28,14 +28,14 @@ export default function Header() {
       <div className="flex items-center gap-x-4">
         <Button
           variant={"outline"}
-          className="rounded-full primary-button hover:text-[#7b3cb6]"
+          className="rounded-full primary-button hover:bg-[#5139edb5] hover:text-white transition-colors duration-300"
         >
           Meet Our Team
         </Button>
         <ProfileDropdown />
         <Button
           variant={"outline"}
-          className="rounded-full primary-button hover:text-[#7b3cb6]"
+          className="rounded-full primary-button hover:bg-[#5139edb5] hover:text-white transition-colors duration-300"
         >
           Connect
         </Button>
