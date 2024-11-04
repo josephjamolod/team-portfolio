3fc4d3e4d8dd88951cde970a1ba9711fbf140deb
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-04 18:33:58 +0800

    create login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,45 @@
+import BackGround from "@/components/auth-components/backGround";
+import { LogInForm } from "@/components/auth-components/loginForm";
+import Image from "next/image";
+
+//font
+import { Roboto_Condensed } from "next/font/google";
+import { cn } from "@/lib/utils";
+export const fonts = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+//pngs
+import purpleBG from "@/public/assets/images/purpleBG.png";
+
+export default function LogInPage() {
+  return (
+    <main className="flex justify-center  min-h-screen p-4">
+      <div className="flex  h-auto  max-w-[900px] w-full max-h-[583px] mt-20 rounded-md relative ">
+        <div className=" w-full absolute h-full object-cover ">
+          <Image
+            src={purpleBG}
+            alt="back-ground"
+            className="h-full rounded-md"
+          />
+        </div>
+
+        <div className="z-10 hidden md:flex flex-1 flex-col gap-y-4 items-center justify-center  rounded-md overflow-hidden text-white">
+          <BackGround>
+            <h1 className={cn(fonts.className, "text-5xl font-black pt-0 ")}>
+              Sign In
+            </h1>
+          </BackGround>
+        </div>
+        <div className="flex flex-1 items-center justify-center z-10">
+          <LogInForm>
+            <h1 className={cn(fonts.className, "text-3xl font-black pt-0 ")}>
+              Log In Here
+            </h1>
+          </LogInForm>
+        </div>
+      </div>
+    </main>
+  );
+}
