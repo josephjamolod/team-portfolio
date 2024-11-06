aab65c7f85ef3075516996bbeab003a7d81c4b6b
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-06 20:44:38 +0800

    create userProvider using useContext

--- providers/userProvider.tsx
+++ providers/userProvider.tsx
@@ -1,10 +1,72 @@
-import { firebaseAuth } from "@/src/lib/firebase/config/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+"use client";
 
-export function useUserContext() {
-  const [user] = useAuthState(firebaseAuth);
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User as FirebaseUser } from "firebase/auth";
+import { firebaseAuth } from "@/src/lib/firebase/config/firebase";
+
+// Define a type for your user, matching the properties provided by Firebase
+type User = {
+  uid: string;
+  email: string | null;
+  displayName: string | null;
+  photoURL: string | null;
+};
+
+export type UserProviderContextType = {
+  user: User | null;
+  isLoading: boolean;
+};
+
+// Create the AuthContext object
+const AuthContext = createContext<UserProviderContextType | undefined>(
+  undefined
+);
+
+// Create a provider component
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null); // State to hold the user object
+  const [isLoading, setIsLoading] = useState(true); // State to track loading status
+
+  // Check for user's authentication status on mount
+  useEffect(() => {
+    // This function from Firebase gets called whenever the auth state changes
+    const unsubscribe = onAuthStateChanged(
+      firebaseAuth,
+      (firebaseUser: FirebaseUser | null) => {
+        if (firebaseUser) {
+          // Map the FirebaseUser object to your custom User type
+          const user: User = {
+            uid: firebaseUser.uid,
+            email: firebaseUser.email,
+            displayName: firebaseUser.displayName,
+            photoURL: firebaseUser.photoURL,
+          };
+          setUser(user);
+        } else {
+          setUser(null);
+        }
+        setIsLoading(false);
+      }
+    );
+
+    // Clean up the subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+// Custom hook to use the AuthContext
+export function useAuth() {
+  const user = useContext(AuthContext);
   if (!user) {
-    return null;
+    throw new Error(
+      "useUserContext must be used within an UserContextProvider"
+    );
   }
   return user;
 }
