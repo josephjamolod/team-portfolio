dcfee732b4747b01f85bba46764bd552c6af2bf4
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-06 21:47:59 +0800

    complete loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Loading() {
-  return <div className="pt-20">loading...</div>;
+  return (
+    <div className="banter-loader ">
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+    </div>
+  );
 }
