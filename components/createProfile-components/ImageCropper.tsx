e3da5703f9099d977587dd1516d9432d2e905d9c
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-10 22:46:36 +0800

    create image crop functionality

--- components/createProfile-components/ImageCropper.tsx
+++ components/createProfile-components/ImageCropper.tsx
@@ -1,19 +1,56 @@
 "use client";
 
-import { ChangeEvent } from "react";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
 import { ModalPropType } from "./modal";
+import ReactCrop, { centerCrop, Crop, makeAspectCrop } from "react-image-crop";
+import Image from "next/image";
+
+const ASPECT_RATIO = 1;
+const MIN_DIMENSION = 150;
 
 const ImageCropper = ({ closeModal, updateAvatar }: ModalPropType) => {
+  const [imgSrc, setImgSrc] = useState("");
+  const [crop, setCrop] = useState<Crop>();
+  const [error, setError] = useState("");
   const onSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
     const reader = new FileReader();
     reader.addEventListener("load", () => {
+      if (error) setError("");
       const imageUrl = reader.result?.toString() || "";
-      console.log(imageUrl);
+      const imageElement = document.createElement("img") as HTMLImageElement;
+      imageElement.src = imageUrl;
+
+      imageElement.addEventListener("load", (e) => {
+        const { naturalWidth, naturalHeight } =
+          e.currentTarget as HTMLImageElement;
+        if (naturalWidth < MIN_DIMENSION || naturalHeight < MIN_DIMENSION) {
+          setError("Image must be at least 150 x 150 pixels.");
+          return setImgSrc("");
+        }
+      });
+      setImgSrc(imageUrl);
     });
     reader.readAsDataURL(file);
+  };
+
+  const onImageLoad = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    const { width, height } = e.currentTarget;
+    const cropWidthInPercent = (MIN_DIMENSION / width) * 100;
+
+    const crop = makeAspectCrop(
+      {
+        unit: "%",
+        width: cropWidthInPercent,
+      },
+      ASPECT_RATIO,
+      width,
+      height
+    );
+    const centeredCrop = centerCrop(crop, width, height);
+    setCrop(centeredCrop);
   };
 
   return (
@@ -26,6 +63,30 @@ const ImageCropper = ({ closeModal, updateAvatar }: ModalPropType) => {
           className="block w-full cursor-pointer text-sm text-slate-500 file:mr-4 file:py-1 file:px-2 file:rounded-full file:border-0 file:text-xs file:bg-gray-700 file:text-sky-300 hover:file:bg-gray-600"
         />
       </label>
+      {imgSrc && (
+        <div className="flex flex-col items-center z-50">
+          <ReactCrop
+            crop={crop}
+            onChange={(pixelCrop, percentCrop) => setCrop(percentCrop)}
+            circularCrop
+            keepSelection
+            aspect={ASPECT_RATIO}
+            minWidth={MIN_DIMENSION}
+          >
+            <Image
+              src={imgSrc}
+              alt="Upload"
+              width={500}
+              height={500}
+              onLoad={onImageLoad}
+              priority
+            />
+          </ReactCrop>{" "}
+          <button className="text-white font-mono text-xs py-2 px-4 rounded-2xl mt-4 bg-sky-500 hover:bg-sky-600">
+            Crop Image
+          </button>
+        </div>
+      )}
     </>
   );
 };
