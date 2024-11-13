6eb67869a3f54e181a33db7142b2b185c6b3d4b0
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-13 21:19:19 +0800

    create the "CreateProfile component" for cover photo and profile

--- app/(private)/create-profile/page.tsx
+++ app/(private)/create-profile/page.tsx
@@ -4,8 +4,11 @@ import CreateProfile from "@/components/createProfile-components/createProfile";
 
 export default function Profile() {
   return (
-    <div className="pt-20">
-      <CreateProfile />
+    <div className="flex items-center flex-col min-h-screen px-10 lg:px-20 3xl:px-60">
+      <div className="flex relative h-[500px] max-w-[1250px] w-full justify-center ">
+        <CreateProfile profile={false} />
+        <CreateProfile profile={true} />
+      </div>
     </div>
   );
 }
