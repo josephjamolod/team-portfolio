0c61aa40dc5d5a74ff7fb10ee262af74c66388d5
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-24 15:55:37 +0800

    create AutoCarousel component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import About from "@/components/main-components/about";
 import Services from "@/components/main-components/services";
 import TeamMVC from "@/components/main-components/teamMVC";
 import Footer from "@/components/main-components/footer";
+import { AutoCarousel } from "@/components/main-components/auto-carousel";
 
 export default function Main() {
   return (
     <main className="h-auto overflow-hidden">
       <div>
         <About />
+        <AutoCarousel />
         <Services />
         <ChooseUs />
         <TeamMVC />
