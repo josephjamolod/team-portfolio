4b9c56b056ffccef3211121d5a24b38623492cc3
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-24 15:53:15 +0800

    improve ui in about page

--- components/main-components/about.tsx
+++ components/main-components/about.tsx
@@ -6,31 +6,45 @@ export default function About() {
   return (
     <div
       id="about-us"
-      className="w-full relative  overflow-hidden pt-28 md:pt-40 px-10 md:px-20 3xl:px-60 flex lg:pb-20 "
+      className="w-full relative  overflow-hidden pt-28 pb-5 md:pt-40 px-10 md:px-20 3xl:px-60 flex lg:pb-20 "
     >
-      <div className="flex flex-col w-full xl:w-1/2 z-10 gap-y-5 md:gap-y-10">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
-          ABOUT US :<br />
-          <span className="text-black">DOLOMAJ&#39;s </span>
-          <span className="font-light italic text-[#5139edb5]">services</span>
-        </h1>
-        <p className="w-4/5 text-base lg:text-xl leading-loose tracking-wider font-normal">
-          Welcome to DOLOMAJs, your trusted partner in your business
-          development. We specialize in providing comprehensive services that
-          empower businesses to streamline their operations, enhance
-          productivity, and achieve their strategic goals. Our expertise spans
-          across various domains, including video editing, executive assistance,
-          web design, lead generation, content writing, and social media
-          management.
-        </p>
-        <div className="flex gap-x-2 w-full md:w-3/4">
+      <div className="flex flex-col  w-full xl:w-1/2 z-10 gap-y-5 md:gap-y-10">
+        <div className="text-4xl flex flex-col gap-y-5 md:text-5xl lg:text-6xl font-bold leading-tight">
+          {/* ABOUT US :<br /> */}
+          <span className="text-primary text-5xl md:text-7xl font-semibold">
+            DOLOMAJ&#39;s{" "}
+            <span className="font-light text-5xl md:text-7xl italic  gradient-text">
+              services.
+            </span>
+          </span>
+
+          <p className="leading-none text-[30px] md:text-[56px]">
+            Welcome to <span className=" gradient-text"> DOLOMAJs</span>, Your
+            trusted partner in your business development.
+            <span className="block"></span>
+            {/* We specialize in providing comprehensive services that
+            empower businesses to streamline their operations, enhance
+            productivity, and achieve their strategic goals. Our expertise spans
+            across various domains, including video editing, executive
+            assistance, web design, lead generation, content writing, and social
+            media management. */}
+          </p>
+        </div>
+
+        <div className="flex pt-2 gap-x-4 w-full md:w-3/4">
           <Button
             variant={"outline"}
-            className="flex-1  primary-button hover:bg-[#5139edb5] hover:text-white transition-colors duration-300"
+            className="flex-1 border border-[#5139ed]  double-gradient-border text-lg md:text-xl  h-12 md:h-14 hover:bg-[#efecff] "
           >
-            Our Portfolio
+            <span className=" w-full h-full flex items-center justify-center scale-100 hover:scale-110 transform transition-transform duration-300 ">
+              {" "}
+              Our Portfolio
+            </span>
           </Button>
-          <Button variant={"outline"} className="flex-1 hover:text-[#5139edb5">
+          <Button
+            variant={"outline"}
+            className="flex-1 hover:border-[#5139edb5] text-lg md:text-xl hover:text-[#5139ed] hover:bg-[#efecff] bg-[#5139edb5] text-secondary  rounded-full h-11 md:h-14"
+          >
             Learn More
           </Button>
         </div>
