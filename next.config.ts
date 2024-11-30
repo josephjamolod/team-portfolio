fe218da983a0d89f269fd226be8f5e9a0097c816
Joseph Jamolod <josephjamolod@gmail.com>
2024-11-30 12:25:43 +0800

    modify nextjsconfig to accept firebase image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "", // Leave empty for default ports
+        pathname: "/v0/b/**", // Use `**` for wildcard matching
+      },
+    ],
+  },
 };
 
 export default nextConfig;
