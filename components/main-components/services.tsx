01c6d65f2639c0258ebec4dabaefc512c412f779
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-07 19:01:47 +0800

    improve service component UI

--- components/main-components/services.tsx
+++ components/main-components/services.tsx
@@ -1,79 +1,19 @@
-interface servicesType {
-  name: string;
-  description: string;
-}
+import React from "react";
 
-const services: servicesType[] = [
-  {
-    name: "Video Editing",
-    description:
-      "Our skilled editors bring your vision to life with professional video editing services that captivate and engage your audience.",
-  },
-  {
-    name: "Executive Assistance",
-    description:
-      "From managing schedules to coordinating tasks, our executive assistants ensure seamless support for your leadership team.",
-  },
-  {
-    name: "Web Design",
-    description:
-      "We create visually appealing and user-friendly websites that enhance your online presence and drive business growth.",
-  },
-  {
-    name: "Lead Generation",
-    description:
-      "Our lead generation experts help you identify and connect with potential customers, boosting your sales pipeline and revenue.",
-  },
-  {
-    name: "Content Writing",
-    description:
-      "Our talented writers craft compelling content that resonates with your audience and strengthens your brand message.",
-  },
-  {
-    name: "Social Media Management",
-    description:
-      "We manage your social media platforms with strategic content and engagement tactics to grow your online community and drive interaction",
-  },
-];
+import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
+import { services } from "@/contants";
 
-import Link from "next/link";
-import { LuArrowUpRightSquare } from "react-icons/lu";
+export default function Services() {
+  const cards = services.map((card, index) => (
+    <Card key={index} card={card} index={index} />
+  ));
 
-import { DropdownMenuSeparator } from "../ui/dropdown-menu";
-
-export default function Test() {
   return (
-    <main
-      id="services"
-      className="flex flex-col justify-center py-5 md:py-10 gap-y-10 md:gap-y-20 px-10 md:px-20 3xl:px-60"
-    >
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+    <div className="flex flex-col justify-center pt-5  px-10 md:px-20 3xl:px-60 ">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight ">
         Our Services
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2  gap-8  ">
-        {services.map((service, index) => (
-          <div
-            className="border bg-[#efecff]  p-8 rounded-3xl border-[#624ced] gradient-border-none-rounded "
-            key={index}
-          >
-            <div className="flex justify-between text-[#624ced] h-auto w-full">
-              <h1 className=" font-bold  text-4xl bg-gradient-to-r from-[#7d6de4] to-[#624ced] bg-clip-text text-transparent">
-                {service.name}
-              </h1>
-              <Link
-                className="text-3xl block scale-100 hover:scale-110 transform transition-transform duration-300 bg-transparent "
-                href={"/"}
-              >
-                <LuArrowUpRightSquare />
-              </Link>
-            </div>
-            <DropdownMenuSeparator className="bg-primary mt-4" />
-            <p className="md:py-8 text-lg font-normal lg:text-xl leading-loose tracking-wider block text-primary">
-              {service.description}
-            </p>
-          </div>
-        ))}
-      </div>
-    </main>
+      <Carousel items={cards} />
+    </div>
   );
 }
--- contants/index.ts
+++ contants/index.ts
@@ -14,3 +14,50 @@ export const formDefaultVals = {
   whatsappNumber: { countryCode: "PH", number: "" },
   youtubeUrl: "",
 };
+
+import videoEdit from "@/public/assets/images/videoEdit.jpg";
+import assistance from "@/public/assets/images/assistance.jpg";
+import webDesign from "@/public/assets/images/webDesign.png";
+import leadGen from "@/public/assets/images/leadGen.jpg";
+import contWrite from "@/public/assets/images/contWrite.jpg";
+import socialMed from "@/public/assets/images/socialMed.jpg";
+import { Card } from "@/components/ui/apple-cards-carousel";
+
+export const services: Card[] = [
+  {
+    title: "Video Editing",
+    category:
+      "Our skilled editors bring your vision to life with professional video editing services that captivate and engage your audience.",
+    src: videoEdit,
+  },
+  {
+    title: "Executive Assistance",
+    category:
+      "From managing schedules to coordinating tasks, our executive assistants ensure seamless support for your leadership team.",
+    src: assistance,
+  },
+  {
+    title: "Web Design",
+    category:
+      "We create visually appealing and user-friendly websites that enhance your online presence and drive business growth.",
+    src: webDesign,
+  },
+  {
+    title: "Lead Generation",
+    category:
+      "Our lead generation experts help you identify and connect with potential customers, boosting your sales pipeline and revenue.",
+    src: leadGen,
+  },
+  {
+    title: "Content Writing",
+    category:
+      "Our talented writers craft compelling content that resonates with your audience and strengthens your brand message.",
+    src: contWrite,
+  },
+  {
+    title: "Social Media Management",
+    category:
+      "We manage your social media platforms with strategic content and engagement tactics to grow your online community and drive interaction.",
+    src: socialMed,
+  },
+];
