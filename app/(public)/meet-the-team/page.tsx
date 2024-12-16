ab01d405e560e571179aae19d8df0e48d48b805b
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-16 19:12:31 +0800

    change fetch process in "meet-the-team-page"

--- app/(public)/meet-the-team/page.tsx
+++ app/(public)/meet-the-team/page.tsx
@@ -1,44 +1,14 @@
 import { GridBackgroundDemo } from "@/components/gridBackGround";
 import { MainTitle } from "@/components/meet-the-team-components/MainTitle";
 import { ThreeDCard } from "@/components/meet-the-team-components/ThreeDCard";
-import { Staff } from "@/components/searchPerson-components/SearchPerson";
-import { firebaseDb } from "@/src/lib/firebase/config/firebase";
-import { collection, getDocs } from "firebase/firestore";
 import React from "react";
 
-export default async function MeetTheTeam() {
-  let profileData: Staff[] = [];
-
-  try {
-    const usersCollection = collection(firebaseDb, "users");
-    const querySnapshot = await getDocs(usersCollection);
-
-    profileData = querySnapshot.docs.map((doc) => {
-      const data = doc.data();
-      return {
-        id: doc.id,
-        ...data,
-        timestamp: data.timestamp?.toMillis() || null, // Convert to plain number
-      } as Staff;
-    });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-  }
-
+export default function MeetTheTeam() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center  pt-20">
       <GridBackgroundDemo />
-      {/* <div className="absolute h-full w-full dark:visible invisible bg-black -z-[2]"></div> */}
       <MainTitle />
-      <div className="h-full w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  justify-center md:px-10 3xl:px-32 pb-4">
-        {profileData.length > 0 ? (
-          profileData.map((staff) => (
-            <ThreeDCard key={staff.id} staff={staff} />
-          ))
-        ) : (
-          <div className="text-white">No profile data available</div>
-        )}
-      </div>
+      <ThreeDCard />
     </div>
   );
 }
--- components/meet-the-team-components/ThreeDCard.tsx
+++ components/meet-the-team-components/ThreeDCard.tsx
@@ -3,56 +3,80 @@
 import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
-import { Staff } from "../searchPerson-components/SearchPerson";
+
 import { FiMail } from "react-icons/fi";
 import { ActionButton } from "../searchPerson-components/Sidebar/ActionButton";
 import { ViewServices } from "./ViewServices";
 
-export function ThreeDCard({ staff }: { staff: Staff }) {
+import { useAuth } from "@/providers/userProvider";
+import { Button } from "../ui/button";
+
+export function ThreeDCard() {
+  const { staffs, fetchAnotherStaff, showMore, staffsLoading } = useAuth();
   return (
-    <CardContainer className="inter-var w-full">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-secondary dark:border-white/[0.2] border-black/[0.1] w-full sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-        <CardItem
-          translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white capitalize"
+    <>
+      <div className="h-full w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  justify-center md:px-10 3xl:px-32 pb-4">
+        {staffs?.usersData.map((staff) => {
+          return (
+            <CardContainer key={staff.id} className="inter-var w-full">
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-secondary dark:border-white/[0.2] border-black/[0.1] w-full sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white capitalize"
+                >
+                  {staff.name} {staff.lastname}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {staff.position}
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-4 relative">
+                  <Image
+                    src={staff.coverSrc || ""}
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl "
+                    alt="thumbnail"
+                    unoptimized
+                  />
+                  <Image
+                    src={staff.profileSrc || ""}
+                    height="1000"
+                    width="1000"
+                    className="h-36 w-36 absolute -bottom-16 left-1/2 -translate-x-1/2 object-cover rounded-full group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <ViewServices staff={staff} />
+                  <div>
+                    <ActionButton
+                      icon={<FiMail />}
+                      staff={staff}
+                      label="Get In Touch"
+                    />
+                  </div>
+                </div>
+              </CardBody>
+            </CardContainer>
+          );
+        })}
+      </div>
+
+      {showMore && !staffsLoading && (
+        <Button
+          disabled={staffsLoading}
+          type="button"
+          onClick={() => fetchAnotherStaff()}
+          variant={"default"}
+          className="w-fit place-self-center mb-4"
         >
-          {staff.name} {staff.lastname}
-        </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-        >
-          {staff.position}
-        </CardItem>
-        <CardItem translateZ="100" className="w-full mt-4 relative">
-          <Image
-            src={staff.coverSrc || ""}
-            height="1000"
-            width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl "
-            alt="thumbnail"
-            unoptimized
-          />
-          <Image
-            src={staff.profileSrc || ""}
-            height="1000"
-            width="1000"
-            className="h-36 w-36 absolute -bottom-16 left-1/2 -translate-x-1/2 object-cover rounded-full group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
-        </CardItem>
-        <div className="flex justify-between items-center mt-20">
-          <ViewServices staff={staff} />
-          <div>
-            <ActionButton
-              icon={<FiMail />}
-              staff={staff}
-              label="Get In Touch"
-            />
-          </div>
-        </div>
-      </CardBody>
-    </CardContainer>
+          Show More
+        </Button>
+      )}
+    </>
   );
 }
