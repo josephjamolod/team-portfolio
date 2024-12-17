03631d31664c4744b99ab2de578efb5535a43cb9
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-17 19:08:55 +0800

    remove bg-background

--- components/orbit.tsx
+++ components/orbit.tsx
@@ -2,8 +2,8 @@ import { OrbitingCircles } from "./ui/orbiting-circles";
 
 export function Orbit() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg   ">
+      <span className="pointer-events-none whitespace-pre-wrap text-center text-6xl font-semibold leading-none text-transparent ">
         {/* DOLOMAJ */}
       </span>
 
