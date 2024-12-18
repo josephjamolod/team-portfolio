11dab158df0249154da065bf839b5064ff73b8d6
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-18 12:44:29 +0800

    optimize image in threeDcard

--- components/meet-the-team-components/ThreeDCard.tsx
+++ components/meet-the-team-components/ThreeDCard.tsx
@@ -52,7 +52,6 @@ export function ThreeDCard() {
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl "
                       alt="thumbnail"
-                      unoptimized
                     />
                     <Image
                       src={staff.profileSrc || ""}
